ee64ba930971effd28829255b13a72f9d48036c9
Mr Punk da Silva <gustavojs417@gmail.com>
2026-01-28 17:14:06 -0300

    test: Add basic Spring Boot context load test

---
+++ springboot-app/src/test/java/com/mrpunkdasilva/kizunacopilot/KizunaCopilotApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mrpunkdasilva.kizunacopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KizunaCopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test ensures that the Spring application context loads successfully.
+        // If it throws an exception, the context failed to load.
+    }
+
+}
